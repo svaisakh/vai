b4fa616ed330e2f848b695fd260457c2be0e4e63
Vaisakh <svaisakh1994@gmail.com>
2018-01-24 18:47:47 +0530

    Add smoothen()
    Added a rudimentary implementation to smoothen a dataset/signal
    using the Savitzky–Golay filter using the scipy.signal.savgol_filter()
    function.
    
    The aim is to abstract away the implementation (as if it isn't easy
    already!) by turning the window_length into a window_fraction.
    
    A window_fraction of 1 means all datapoints are considered and
    as it tends to 0, one datapoint is considered at a time (conflicts with
    polyorder).

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pytest
+from hypothesis import given
 
-from vai.utils import find_outliers
+from vai.utils import find_outliers, smoothen
+from hypothesis.extra import numpy as nph
+from hypothesis import strategies as st
 
 
 class TestRemoveOutlier:
@@ -38,3 +41,49 @@ class TestRemoveOutlier:
     def test_window_not_greater_than_one(self):
         with pytest.raises(ValueError):
             find_outliers(np.zeros(5), window_fraction=2)
+
+
+class TestSmoothen:
+    def test_cannot_send_none(self):
+        with pytest.raises(TypeError):
+            smoothen(None)
+
+        with pytest.raises(TypeError):
+            smoothen(np.zeros(5), None)
+
+        with pytest.raises(TypeError):
+            smoothen(np.zeros(5), polyorder=None)
+
+    def test_cannot_send_empty(self):
+        with pytest.raises(ValueError):
+            smoothen([])
+
+        with pytest.raises(ValueError):
+            smoothen(np.array([]))
+
+    @given(st.floats())
+    def test_fraction_is_fraction(self, window_fraction):
+        if 0 < window_fraction <= 1:
+            return
+
+        with pytest.raises(ValueError):
+            smoothen(np.zeros(5), window_fraction=window_fraction)
+
+    @given(nph.arrays(nph.floating_dtypes(), nph.array_shapes(max_dims=1, max_side=100)))
+    def test_polyorder_cannot_be_greater_than_or_equal_to_data_length(self, data):
+        polyorder = np.random.randint(len(data), len(data) * 2)
+        with pytest.raises(ValueError):
+            smoothen(data, polyorder=polyorder)
+
+    @given(nph.arrays(nph.floating_dtypes(), nph.array_shapes(max_dims=1, min_side=2, max_side=100)), st.floats(0, 1),
+           st.integers(0, 100))
+    def test_polyorder_cannot_be_greater_than_window_length(self, data, window_fraction, polyorder):
+        window_length = int(window_fraction * len(data))
+        if window_length % 2 == 0:
+            window_length = max(1, window_length - 1)
+
+        if polyorder <= window_length:
+            return
+
+        with pytest.raises(ValueError):
+            smoothen(data, window_fraction, polyorder)
--- vai/utils.py
+++ vai/utils.py
@@ -1,8 +1,12 @@
+import warnings
+
 import numpy as np
+from scipy.signal import savgol_filter
 
 
 def find_outliers(data, threshold=3.5, window_fraction=0.05):
-    """Based on http://www.itl.nist.gov/div898/handbook/eda/section3/eda35h.htm"""
+    """Based on http://www.itl.nist.gov/div898/handbook/eda/section3
+    /eda35h.htm """
 
     def __handle_args():
         if len(data) == 0:
@@ -10,25 +14,36 @@ def find_outliers(data, threshold=3.5, window_fraction=0.05):
         if len(data) < 3:
             return data, [False] * len(data)
         if len(data.shape) == 1:
-            return find_outliers(np.expand_dims(data, -1), threshold, window_fraction)
+            return find_outliers(np.expand_dims(data, -1), threshold,
+                                 window_fraction)
         if threshold is None:
             raise ValueError('threshold cannot be None')
         if threshold < 0:
-            raise ValueError('threshold should be non negative but got {}'.format(threshold))
+            raise ValueError(
+                'threshold should be non negative but got {}'.format(
+                    threshold))
         elif np.isinf(threshold) or np.isnan(threshold):
-            raise ValueError('threshold should be a finite number but got {}'.format(threshold))
+            raise ValueError(
+                'threshold should be a finite number but got {}'.format(
+                    threshold))
 
         if window_fraction < 0 or window_fraction > 1:
-            raise ValueError('window_fraction should be a fraction (duh!). But got {}'.format(window_fraction))
+            raise ValueError(
+                'window_fraction should be a fraction (duh!). But got '
+                '{}'.format(
+                    window_fraction))
         elif np.isinf(window_fraction) or np.isnan(window_fraction):
-            raise ValueError('threshold should be a finite number but got {}'.format(threshold))
+            raise ValueError(
+                'threshold should be a finite number but got {}'.format(
+                    threshold))
 
     arg_err = __handle_args()
     if arg_err is not None:
         return arg_err
 
     # Subdivide data into small windows
-    window_length = max(int(len(data) * window_fraction), 3) if window_fraction is not None else 3
+    window_length = max(int(len(data) * window_fraction),
+                        3) if window_fraction is not None else 3
     divide_ids = np.arange(window_length, len(data), window_length)
 
     split_data = np.split(data, divide_ids)
@@ -50,3 +65,38 @@ def find_outliers(data, threshold=3.5, window_fraction=0.05):
         return outlier_mask
 
     return np.concatenate([_find_outliers(d) for d in split_data])
+
+
+def smoothen(data, window_fraction=0.3, polyorder=3):
+    def __handle_args():
+        if type(data) is not np.ndarray and type(data) is not list:
+            raise TypeError('data needs to be a list or numpy array. Got {}'.format(type(data)))
+        if len(data) == 0:
+            raise ValueError('data is empty!')
+
+        if type(window_fraction) is not float:
+            raise TypeError('window_fraction should be a fraction (duh!). But got {}'.format(type(window_fraction)))
+        if window_fraction < 0 or window_fraction > 1:
+            raise ValueError('window_fraction should be a fraction (duh!). But got {}'.format(window_fraction))
+        if np.isinf(window_fraction) or np.isnan(window_fraction):
+            raise ValueError('window_fraction should be a finite number but got {}'.format(window_fraction))
+
+        if type(polyorder) is not int:
+            raise TypeError('polyorder needs to be a non-negative integer but got {}'.format(type(polyorder)))
+        if polyorder < 0:
+            raise ValueError('polyorder needs to be a non-negative integer but got {}'.format(polyorder))
+
+    arg_err = __handle_args()
+    if arg_err is not None:
+        return arg_err
+
+    window_length = int(len(data) * window_fraction)
+    # savgol_filter needs an odd window_length
+    if window_length % 2 == 0:
+        window_length = max(window_length - 1, 1)
+
+    if polyorder >= window_length:
+        raise ValueError('polyorder ({}) needs to be lower or window_frac ({}) needs to be higher'.format(polyorder,
+                                                                                                          window_fraction))
+
+    return savgol_filter(data, window_length, polyorder)
