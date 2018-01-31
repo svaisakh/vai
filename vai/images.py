38578bc7d1a75d0bdaac6cc121ebef721a1015b7
Vaisakh <svaisakh1994@gmail.com>
2018-01-31 19:21:49 +0530

    Add Method to Show an Image
    Along with some embellishments, of course.

--- vai/images.py
+++ vai/images.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 from scipy.misc import imresize
 
 
@@ -67,3 +69,53 @@ def _resize_images(images, shape='smean', interp='bilinear', mode=None):
         return err_arg
 
     return [imresize(image, shape, interp, mode) for image in images]
+
+
+def _show_image(image, **kwargs):
+    title = kwargs.pop('title', None)
+    pixel_range = kwargs.pop('pixel_range', (0, 255))
+    cmap = kwargs.pop('cmap', None)
+    ax = kwargs.pop('ax', None)
+    retain = kwargs.pop('retain', False)
+
+    def _handle_args():
+        nonlocal pixel_range, ax
+        if type(image) is not np.ndarray:
+            raise TypeError('image needs to be a numpy array. Found {}'.format(type(image)))
+        if len(image.shape) not in (2, 3):
+            raise ValueError('invalid image dimensions. Needs to be 2 or 3-D. Found {}'.format(len(image.shape)))
+
+        if title is not None and type(title) is not str:
+            raise TypeError('title needs to be None or a valid string. Found {}'.format(title))
+
+        if type(pixel_range) not in [tuple, list, np.ndarray]:
+            if type(pixel_range) is str:
+                if pixel_range == 'auto':
+                    pixel_range = (image.min(), image.max())
+                else:
+                    raise ValueError('pixel_range should be auto. Found {}'.format(pixel_range))
+            else:
+                raise TypeError("pixel_range needs to be a tuple, list, numpy array or 'auto'."
+                                " Found {}".format(type(pixel_range)))
+        elif len(pixel_range) != 2:
+            raise ValueError('pixel_range needs to be of size 2 - (min, max). Found size {}'.format(len(pixel_range)))
+
+        if ax is None:
+            ax = plt.subplots()[1]
+
+        if type(retain) is not bool:
+            raise TypeError('retain must be either True or False')
+
+    err_arg = _handle_args()
+    if err_arg is not None:
+        return err_arg
+
+    ax.imshow(((image - pixel_range[0]) / (pixel_range[1] - pixel_range[0])), cmap, vmin=0, vmax=1, **kwargs)
+    if title is not None:
+        ax.set_title(title)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.grid('off')
+
+    if not retain:
+        plt.show()
