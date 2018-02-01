bbdc6caf52cefbaa857a03cedbbf0780517e3f0d
Vaisakh <svaisakh1994@gmail.com>
2018-02-01 09:55:45 +0530

    Add setup.py and setup.cfg

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'vaipy',
+  packages = ['vai'], 
+  version = '0.0.1',
+  description = 'Personally Handcrafted Library for AI',
+  author = 'Vaisakh',
+  author_email = 'svaisakh1994@gmail.com',
+  url = 'https://github.com/svaisakh/vaipy', # use the URL to the github repo
+  download_url = 'https://github.com/svaisakh/vaipy/archive/0.0.1.tar.gz', # I'll explain this in a second
+  keywords = ['AI', 'Machine', 'Learning', 'Python', 'Library', 'Jupyter',  'Notebook'], # arbitrary keywords
+  classifiers = [],
+)
